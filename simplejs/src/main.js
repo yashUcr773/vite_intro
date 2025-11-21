a0c763eb6c56693ac2f040d823b700ba9d6fe73d
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2025-11-21 22:01:07 +0530

    basic config. env vars

--- simplejs/src/main.js
+++ simplejs/src/main.js
@@ -25,6 +25,7 @@ document.querySelector('#app').innerHTML = `
     </a>
     <h1>Hello Vite!</h1>
     <div class="card">
+      <p>Env var: ${import.meta.VITE_SOME_VAL_1}</p>
       <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
@@ -34,3 +35,4 @@ document.querySelector('#app').innerHTML = `
 `;
 
 setupCounter(document.querySelector('#counter'));
+console.log(import.meta.env);
