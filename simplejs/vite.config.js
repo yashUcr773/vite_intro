5290f545c5d98c13019b5b4ca8d1916be958e37c
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2025-11-21 22:08:18 +0530

    multiple routes

--- simplejs/vite.config.js
+++ simplejs/vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   build: {
     minify: false,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, './admin/index.html'),
+      },
+    },
   },
   server: {
     port: 4000,
