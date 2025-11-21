1fc859ee4d8d58daa84fb12956e04ed75a6398f2
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2025-11-21 22:17:06 +0530

    bundle splitting

--- reactApp/vite.config.ts
+++ reactApp/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+function makeChunks(id: string) {
+  if (id.includes('node_modules/react')) return 'react';
+  if (id.includes('node_modules')) return 'vendor';
+  return 'main';
+}
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: { manualChunks: makeChunks },
+    },
+  },
+});
